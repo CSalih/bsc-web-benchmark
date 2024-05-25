2b7264bf674b1d35c42be271b6822ca12648b645
Salih Candir <salih@live.at>
2024-05-25 21:47:14 +0200

    ci: update angular from 17 to 18

--- apps/app-angular/src/app/app.component.ts
+++ apps/app-angular/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   template: `
     <main>
         <h1>Hello World</h1>
     </main>
   `,
-  styles: [],
 })
 export class AppComponent { }
