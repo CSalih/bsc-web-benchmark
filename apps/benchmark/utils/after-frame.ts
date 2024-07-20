c9ca6643ae36c05407946f940ec7e891d044401f
Salih Candir <salih@live.at>
2024-07-20 10:56:41 +0200

    wip bench

--- apps/benchmark/utils/after-frame.ts
+++ apps/benchmark/utils/after-frame.ts
@@ -1,23 +1,20 @@
+declare global {
+  interface Window {
+    afterFrame: (callback: () => void) => void;
+  }
+}
+
 export const afterFrame = () => {
-  // Source: https://github.com/andrewiggins/afterframe
-  let callbacks = [];
-  let channel = new MessageChannel();
-  let postMessage = function () {
-    this.postMessage(undefined);
-  }.bind(channel.port2);
+  const channel = new MessageChannel();
+  const callbacks = [];
+  const port = channel.port2;
   channel.port1.onmessage = () => {
-    let toFlush = callbacks;
-    callbacks = [];
-    let time = performance.now();
-    for (let i = 0; i < toFlush.length; i++) {
-      toFlush[i](time);
-    }
+    callbacks.splice(0, callbacks.length).forEach((cb) => cb());
   };
-  channel = null;
-  // @ts-ignore: We register the function in the window object
-  window.afterFrame = function (callback) {
-    if (callbacks.push(callback) === 1) {
-      requestAnimationFrame(postMessage);
-    }
+  const postMessage = () => {
+    port.postMessage(null);
+  };
+  window.afterFrame = (callback) => {
+    callbacks.push(callback) === 1 && requestAnimationFrame(postMessage);
   };
 };
