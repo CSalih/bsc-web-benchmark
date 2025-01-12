15d158a276d6dd83465eb5952b465bc54fc533f9
Salih Candir <salih@live.at>
2025-01-12 20:51:53 +0100

    feat: clean up

--- benchmark/benchmark-playwright/utils/base-url.ts
+++ benchmark/benchmark-playwright/utils/base-url.ts
@@ -1,10 +1,1 @@
-const app = {
-  baseUrl: {
-    angular: "http://localhost:3000",
-    leptos: "http://localhost:3001",
-    react: "http://localhost:3002",
-    vue: "http://localhost:3003",
-  },
-};
-
 export const baseUrl = (path: string) => path;
