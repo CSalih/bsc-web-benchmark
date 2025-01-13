769480002bfd833d5d3785f67e707eae6753db53
Salih Candir <salih@live.at>
2025-01-14 00:22:18 +0100

    refactor: clean up

--- apps/realworld-leptos/src/auth/mod.rs
+++ apps/realworld-leptos/src/auth/mod.rs
@@ -1,8 +1,5 @@
 use leptos::prelude::*;
-
-mod api;
-
-pub use api::*;
+use serde::{Deserialize, Serialize};
 
 pub type LogoutAction = Action<(), bool>;
 
@@ -15,8 +12,27 @@ pub struct AuthContext {
 impl AuthContext {
     pub(crate) fn new(
         user: ReadSignal<Option<crate::models::User>>,
-        access_token: ReadSignal<Option<String>>
+        access_token: ReadSignal<Option<String>>,
     ) -> Self {
         Self { user, access_token }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct LoginUser {
+    pub email: String,
+    pub token: String,
+    pub username: String,
+    pub bio: Option<String>,
+    pub image: Option<String>,
+}
+
+impl Into<crate::models::User> for LoginUser {
+    fn into(self) -> crate::models::User {
+        crate::models::User::new(self.username, self.email)
+            .set_bio(self.bio.unwrap_or_default())
+            .unwrap()
+            .set_image(self.image.unwrap_or_default())
+            .unwrap()
     }
 }
