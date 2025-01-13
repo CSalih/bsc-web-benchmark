260915b6ad5294d243dd15d7e88a64f4e13bd82d
Salih Candir <salih@live.at>
2025-01-13 23:54:06 +0100

    feat: implement signup feature

--- apps/realworld-leptos/src/pages/signup.rs
+++ apps/realworld-leptos/src/pages/signup.rs
@@ -1,32 +1,92 @@
-use crate::auth::{SignupCommand, SignupResponse, SignupSignal};
+use crate::auth::LoginUser;
 use leptos::prelude::*;
+use leptos::task::spawn_local;
 use leptos_router::*;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use crate::models::User;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SignupCommand {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct  SignupResponse {
+    pub user: LoginUser,
+}
+
+pub fn validate_signup(signup_command: &SignupCommand) -> Result<User, String> {
+    User::default()
+        .set_username(signup_command.username.clone())?
+        .set_password(signup_command.password.clone())?
+        .set_email(signup_command.email.clone())
+}
 
 #[component]
-pub fn SignupPage(signup: SignupSignal) -> impl IntoView {
-    let result_of_call = signup.value();
+pub fn SignupPage() -> impl IntoView {
+    let (signup_success, set_signup_success) = signal(false);
+    let (error_message, set_error_message) = signal(String::from(""));
+    let (email, set_email) = signal(String::from(""));
+    let (username, set_username) = signal(String::from(""));
+    let (password, set_password) = signal(String::from(""));
 
-    let error = move || {
-        result_of_call.with(|msg| {
-            msg.as_ref()
-                .map(|inner| match inner {
-                    Ok(SignupResponse::Success) => {
-                        let navigate = hooks::use_navigate();
-                        navigate("/login", NavigateOptions::default());
+    let signup = move |username: String, email: String, password: String| async move {
+        let signup_command = SignupCommand { username, email, password };
+        if let Err(err) = validate_signup(&signup_command) {
+            set_error_message.set(err);
+            return;
+        }
 
-                        "Done".into()
-                    }
-                    Ok(SignupResponse::ValidationError(x)) => {
-                        format!("Problem while validating: {x}")
-                    }
-                    Ok(SignupResponse::CreateUserError(x)) => {
-                        format!("Problem while creating user: {x}")
-                    }
-                    Err(_) => "There was a problem, try again later".into(),
-                })
-                .unwrap_or_default()
-        })
+        let url = "http://localhost:8080/api/users";
+        let mut json_body = std::collections::HashMap::new();
+        json_body.insert("user", signup_command);
+
+        let client = reqwest::Client::new();
+        let res = client.post(url).json(&json_body).send().await;
+
+        let Ok(res) = res else {
+            set_error_message.set("unknown error".to_string());
+            return;
+        };
+        if !res.status().is_success() {
+            let error_message = res
+                .json::<Value>()
+                .await
+                .ok()
+                .and_then(|json| json.get("error").and_then(|msg| msg.as_str().map(String::from)))
+                .unwrap_or_else(|| "Unknown error".to_string());
+            set_error_message.set(error_message);
+            return;
+        }
+
+        let signup_res = res
+            .bytes()
+            .await
+            .ok()
+            .map(|bytes| serde_json::from_slice::<Value>(&bytes).ok())
+            .flatten();
+
+        match signup_res {
+            Some(login_res) => {
+                let _ = serde_json::from_value::<SignupResponse>(login_res).unwrap();
+                set_signup_success.set(true);
+            }
+            None => {
+                // Response is not valid json
+                set_error_message.set("unknown error".to_string());
+            }
+        }
     };
+
+    Effect::new(move || {
+        if signup_success.get() {
+            let navigate = hooks::use_navigate();
+            navigate("/login", NavigateOptions::default());
+        }
+    });
 
     view! {
         <div class="auth-page">
@@ -38,13 +98,17 @@ pub fn SignupPage(signup: SignupSignal) -> impl IntoView {
                             <a href="/login">"Have an account?"</a>
                         </p>
 
-                        <p class="error-messages text-xs-center">{error}</p>
+                        <p class="error-messages text-xs-center">{error_message}</p>
 
                         <form on:submit=move |e| {
                             e.prevent_default();
-                            if let Ok(data) = SignupCommand::from_event(&e) {
-                                signup.dispatch(data);
-                            }
+                            spawn_local(async move {
+                                signup(
+                                    username.get_untracked(),
+                                    email.get_untracked(),
+                                    password.get_untracked()
+                                ).await;
+                            });
                         }>
                             <fieldset class="form-group">
                                 <input
@@ -53,6 +117,7 @@ pub fn SignupPage(signup: SignupSignal) -> impl IntoView {
                                     type="text"
                                     placeholder="Your Username"
                                     required=true
+                                    bind:value=(username, set_username)
                                 />
                             </fieldset>
                             <fieldset class="form-group">
@@ -62,6 +127,7 @@ pub fn SignupPage(signup: SignupSignal) -> impl IntoView {
                                     type="email"
                                     placeholder="Email"
                                     required=true
+                                    bind:value=(email, set_email)
                                 />
                             </fieldset>
                             <fieldset class="form-group">
@@ -71,6 +137,7 @@ pub fn SignupPage(signup: SignupSignal) -> impl IntoView {
                                     type="password"
                                     placeholder="Password"
                                     required=true
+                                    bind:value=(password, set_password)
                                 />
                             </fieldset>
                             <button type="submit" class="btn btn-lg btn-primary pull-xs-right">
