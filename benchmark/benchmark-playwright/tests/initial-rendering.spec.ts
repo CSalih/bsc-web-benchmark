74f587ec8dd65e016abd00af715334463a34e084
Salih Candir <salih@live.at>
2025-01-20 20:15:49 +0100

    feat: remove analytic service and dump web vital results to csv file

--- benchmark/benchmark-playwright/tests/initial-rendering.spec.ts
+++ benchmark/benchmark-playwright/tests/initial-rendering.spec.ts
@@ -1,16 +1,11 @@
 import { resolve } from "path";
 import { expect, test } from "@playwright/test"
 import { baseUrl } from "../utils/base-url";
+import { persistWebVital } from "../utils/persist-measure"
 
 test.describe("initial rendering phase", () => {
   test("web_vitals", async ({ page }) => {
-    // Ignore analytics when warming up
-    await page.route("http://localhost:8000/api/v1/event", (route) => {
-      route.fulfill({ status: 204 });
-    });
-
     await page.addInitScript({
-      // TODO: may parse script and replace project name, environment and version
       path: resolve(__dirname, "../utils/web-vitals.js"),
     });
 
@@ -23,9 +18,19 @@ test.describe("initial rendering phase", () => {
       name: "Playwright"
     })).toBeAttached();
 
-    // click on an element to get Web Vitals like INP
+    // Click on a link to get Web Vitals like INP
     await page.getByRole("heading", {
-      name: "conduit"
+      name: "Playwright"
     }).click();
+    await expect(page.getByRole("heading", {
+      name: "Summary of Playwright"
+    })).toBeVisible();
+
+    // Persist the Web Vitals
+    const results = await page.evaluate(() => {
+      // @ts-ignore: webVitals is added by the injected script
+      return window.webVitals;
+    });
+    persistWebVital(page, results);
   });
 });
