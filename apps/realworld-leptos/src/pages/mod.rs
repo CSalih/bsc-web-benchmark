031477fb142eb99b9fed341a1725bb89aade186b
Salih Candir <salih@live.at>
2025-01-23 22:31:43 +0100

    refactor: remove page

--- apps/realworld-leptos/src/pages/mod.rs
+++ apps/realworld-leptos/src/pages/mod.rs
@@ -3,16 +3,13 @@ mod article;
 mod home;
 mod login;
 // mod profile;
-// mod settings;
 mod settings;
 mod signup;
-// mod reset_password;
 
 pub use article::ArticlePage;
 // pub use editor::*;
 pub use home::*;
 pub use login::*;
 // pub use profile::*;
-// pub use reset_password::*;
 pub use settings::SettingsPage;
 pub use signup::*;
