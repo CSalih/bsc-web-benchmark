d70c91d9a1dba803dcdd6788820b8f61d349f099
Salih Candir <salih@live.at>
2025-02-03 14:30:23 +0100

    refactor: replace `VITE_API_HOST` with `VITE_API_URL`

--- apps/realworld-react/src/agent.js
+++ apps/realworld-react/src/agent.js
@@ -1,5 +1,5 @@
 const API_ROOT =
-  import.meta.env.VITE_API_HOST ?? "http://localhost:8080/api";
+  import.meta.env.VITE_API_URL ?? "http://localhost:8080/api";
 
 /**
  * Serialize object to URL params
