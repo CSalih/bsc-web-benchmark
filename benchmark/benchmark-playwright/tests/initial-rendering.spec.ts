0b4d932dc2a1545ecf550721a99c1c17236f8209
Salih Candir <salih@live.at>
2025-03-15 14:41:49 +0100

    fix: wait one second to avoid missing metrics

--- benchmark/benchmark-playwright/tests/initial-rendering.spec.ts
+++ benchmark/benchmark-playwright/tests/initial-rendering.spec.ts
@@ -1,40 +1,43 @@
-import { resolve } from "path";
-import { expect, test } from "@playwright/test";
-import { baseUrl } from "../utils/base-url";
-import { persistWebVital } from "../utils/persist-measure";
+import { resolve } from "path"
+import { expect, test } from "@playwright/test"
+import { baseUrl } from "../utils/base-url"
+import { persistWebVital } from "../utils/persist-measure"
 
 test.describe("initial rendering phase", () => {
   test("web_vitals", async ({ page }) => {
     await page.addInitScript({
       path: resolve(__dirname, "../utils/web-vitals.js"),
-    });
+    })
 
-    await page.goto(baseUrl("/"));
+    await page.goto(baseUrl("/"))
 
     // Validate articles are loaded and rendered to the DOM
     await expect(
       page.getByRole("heading", {
         name: "Playwright",
       }),
-    ).toBeAttached();
+    ).toBeAttached()
 
     // Click on a link to get Web Vitals like INP
     await page
       .getByRole("heading", {
         name: "Playwright",
       })
-      .click();
+      .click()
     await expect(
       page.getByRole("heading", {
         name: "Summary of Playwright",
       }),
-    ).toBeVisible();
+    ).toBeVisible()
+
+    // Wait for a second so metrics are available
+    await page.waitForTimeout(1000)
 
     // Persist the Web Vitals
     const results = await page.evaluate(() => {
       // @ts-ignore: webVitals is added by the injected script
-      return window.webVitals;
-    });
-    persistWebVital(page, results);
-  });
-});
+      return window.webVitals
+    })
+    persistWebVital(page, results)
+  })
+})
--- benchmark/benchmark-playwright/tests/responsiveness.spec.ts
+++ benchmark/benchmark-playwright/tests/responsiveness.spec.ts
@@ -1,30 +1,30 @@
-import { test } from "@playwright/test";
-import { afterFrame } from "../utils/after-frame";
-import { baseUrl } from "../utils/base-url";
-import { persistMeasure } from "../utils/persist-measure";
+import { test } from "@playwright/test"
+import { afterFrame } from "../utils/after-frame"
+import { baseUrl } from "../utils/base-url"
+import { persistMeasure } from "../utils/persist-measure"
 
-test.describe.configure({ mode: "parallel" });
+test.describe.configure({ mode: "parallel" })
 
 test("create_1000_rows", async ({ page }) => {
   // Add custom script to measure performance
-  await page.addInitScript(afterFrame);
+  await page.addInitScript(afterFrame)
 
   // open page
-  await page.goto(baseUrl("/"));
+  await page.goto(baseUrl("/"))
 
   // Validate elements are available in the DOM
-  await test.expect(page.locator("#run")).toBeVisible();
-  await test.expect(page.locator(".table > tbody")).toBeEmpty();
+  await test.expect(page.locator("#run")).toBeVisible()
+  await test.expect(page.locator(".table > tbody")).toBeEmpty()
 
   // Create 1000 rows
   await page.evaluate(() => {
-    performance.mark("create_1000_start");
-    document.getElementById("run").click();
+    performance.mark("create_1000_start")
+    document.getElementById("run").click()
     // @ts-ignore: We are sure that the function is available in the window object
     window.afterFrame(() => {
-      performance.mark("create_1000_end");
-    });
-  });
+      performance.mark("create_1000_end")
+    })
+  })
 
   // wait for table to render and validate the number of rows
   await page
@@ -33,8 +33,11 @@ test("create_1000_rows", async ({ page }) => {
     )
     .waitFor({
       state: "attached",
-    });
-  await test.expect(page.locator(".table > tbody > tr")).toHaveCount(1000);
+    })
+  await test.expect(page.locator(".table > tbody > tr")).toHaveCount(1000)
+
+  // Wait for a second so metrics are available
+  await page.waitForTimeout(1000)
 
   // Calculate the duration of the action
   const measure = await page.evaluate(() => {
@@ -42,31 +45,31 @@ test("create_1000_rows", async ({ page }) => {
       "create_1000_duration",
       "create_1000_start",
       "create_1000_end",
-    );
-  });
-  persistMeasure(page, measure);
-});
+    )
+  })
+  persistMeasure(page, measure)
+})
 
 test("create_10000_rows", async ({ page }) => {
   // Add custom script to measure performance
-  await page.addInitScript(afterFrame);
+  await page.addInitScript(afterFrame)
 
   // open page
-  await page.goto(baseUrl("/"));
+  await page.goto(baseUrl("/"))
 
   // Validate elements are available in the DOM
-  await test.expect(page.locator("#runlots")).toBeVisible();
-  await test.expect(page.locator(".table > tbody")).toBeEmpty();
+  await test.expect(page.locator("#runlots")).toBeVisible()
+  await test.expect(page.locator(".table > tbody")).toBeEmpty()
 
   // Create 1000 rows
   await page.evaluate(() => {
-    performance.mark("create_10000_start");
-    document.getElementById("runlots").click();
+    performance.mark("create_10000_start")
+    document.getElementById("runlots").click()
     // @ts-ignore: We are sure that the function is available in the window object
     window.afterFrame(() => {
-      performance.mark("create_10000_end");
-    });
-  });
+      performance.mark("create_10000_end")
+    })
+  })
 
   // wait for table to render and validate the number of rows
   await page
@@ -76,10 +79,13 @@ test("create_10000_rows", async ({ page }) => {
     .waitFor({
       timeout: 10000,
       state: "attached",
-    });
+    })
   await test.expect(page.locator(".table > tbody > tr")).toHaveCount(10000, {
     timeout: 10000,
-  });
+  })
+
+  // Wait for a second so metrics are available
+  await page.waitForTimeout(1000)
 
   // Calculate the duration of the action
   const measure = await page.evaluate(() => {
@@ -87,27 +93,27 @@ test("create_10000_rows", async ({ page }) => {
       "create_10000_duration",
       "create_10000_start",
       "create_10000_end",
-    );
-  });
-  persistMeasure(page, measure);
-});
+    )
+  })
+  persistMeasure(page, measure)
+})
 
 test("append_1000_rows", async ({ page }) => {
   // Add custom script to measure performance
-  await page.addInitScript(afterFrame);
+  await page.addInitScript(afterFrame)
 
   // open page
-  await page.goto(baseUrl("/"));
+  await page.goto(baseUrl("/"))
 
   // Validate elements are available in the DOM
-  await test.expect(page.locator("#add")).toBeVisible();
-  await test.expect(page.locator("#run")).toBeVisible();
-  await test.expect(page.locator(".table > tbody")).toBeEmpty();
+  await test.expect(page.locator("#add")).toBeVisible()
+  await test.expect(page.locator("#run")).toBeVisible()
+  await test.expect(page.locator(".table > tbody")).toBeEmpty()
 
   // Create 1000 rows
   await page.evaluate(() => {
-    document.getElementById("run").click();
-  });
+    document.getElementById("run").click()
+  })
   // wait for table to render and validate the number of rows
   await page
     .locator(
@@ -115,16 +121,16 @@ test("append_1000_rows", async ({ page }) => {
     )
     .waitFor({
       state: "attached",
-    });
+    })
 
   await page.evaluate(() => {
-    performance.mark("append_1000_rows_start");
-    document.getElementById("add").click();
+    performance.mark("append_1000_rows_start")
+    document.getElementById("add").click()
     // @ts-ignore: We are sure that the function is available in the window object
     window.afterFrame(() => {
-      performance.mark("append_1000_rows_end");
-    });
-  });
+      performance.mark("append_1000_rows_end")
+    })
+  })
 
   // wait for table to render and validate the number of rows
   await page
@@ -133,8 +139,11 @@ test("append_1000_rows", async ({ page }) => {
     )
     .waitFor({
       state: "attached",
-    });
-  await test.expect(page.locator(".table > tbody > tr")).toHaveCount(2000);
+    })
+  await test.expect(page.locator(".table > tbody > tr")).toHaveCount(2000)
+
+  // Wait for a second so metrics are available
+  await page.waitForTimeout(1000)
 
   // Calculate the duration of the action
   const measure = await page.evaluate(() => {
@@ -142,27 +151,27 @@ test("append_1000_rows", async ({ page }) => {
       "append_1000_rows_duration",
       "append_1000_rows_start",
       "append_1000_rows_end",
-    );
-  });
-  persistMeasure(page, measure);
-});
+    )
+  })
+  persistMeasure(page, measure)
+})
 
 test("update_every_10th_row", async ({ page }) => {
   // Add custom script to measure performance
-  await page.addInitScript(afterFrame);
+  await page.addInitScript(afterFrame)
 
   // open page
-  await page.goto(baseUrl("/"));
+  await page.goto(baseUrl("/"))
 
   // Validate elements are available in the DOM
-  await test.expect(page.locator("#run")).toBeVisible();
-  await test.expect(page.locator("#update")).toBeVisible();
-  await test.expect(page.locator(".table > tbody")).toBeEmpty();
+  await test.expect(page.locator("#run")).toBeVisible()
+  await test.expect(page.locator("#update")).toBeVisible()
+  await test.expect(page.locator(".table > tbody")).toBeEmpty()
 
   // Create 10000 rows
   await page.evaluate(() => {
-    document.getElementById("run").click();
-  });
+    document.getElementById("run").click()
+  })
   // wait for table to render and validate the number of rows
   await page
     .locator(
@@ -170,17 +179,17 @@ test("update_every_10th_row", async ({ page }) => {
     )
     .waitFor({
       state: "attached",
-    });
-  await test.expect(page.locator(".table > tbody > tr")).toHaveCount(1000);
+    })
+  await test.expect(page.locator(".table > tbody > tr")).toHaveCount(1000)
 
   await page.evaluate(() => {
-    performance.mark("update_every_10th_row_start");
-    document.getElementById("update").click();
+    performance.mark("update_every_10th_row_start")
+    document.getElementById("update").click()
     // @ts-ignore: We are sure that the function is available in the window object
     window.afterFrame(() => {
-      performance.mark("update_every_10th_row_end");
-    });
-  });
+      performance.mark("update_every_10th_row_end")
+    })
+  })
 
   // check if some 10th row is updated
   await test
@@ -189,28 +198,31 @@ test("update_every_10th_row", async ({ page }) => {
         ".table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)",
       ),
     )
-    .toContainText("!!!");
+    .toContainText("!!!")
   await test
     .expect(
       page.locator(
         ".table > tbody:nth-child(1) > tr:nth-child(11) > td:nth-child(2)",
       ),
     )
-    .toContainText("!!!");
+    .toContainText("!!!")
   await test
     .expect(
       page.locator(
         ".table > tbody:nth-child(1) > tr:nth-child(981) > td:nth-child(2)",
       ),
     )
-    .toContainText("!!!");
+    .toContainText("!!!")
   await test
     .expect(
       page.locator(
         ".table > tbody:nth-child(1) > tr:nth-child(991) > td:nth-child(2)",
       ),
     )
-    .toContainText("!!!");
+    .toContainText("!!!")
+
+  // Wait for a second so metrics are available
+  await page.waitForTimeout(1000)
 
   // Calculate the duration of the action
   const measure = await page.evaluate(() => {
@@ -218,27 +230,27 @@ test("update_every_10th_row", async ({ page }) => {
       "update_every_10th_row_duration",
       "update_every_10th_row_start",
       "update_every_10th_row_end",
-    );
-  });
-  persistMeasure(page, measure);
-});
+    )
+  })
+  persistMeasure(page, measure)
+})
 
 test("swap_rows", async ({ page }) => {
   // Add custom script to measure performance
-  await page.addInitScript(afterFrame);
+  await page.addInitScript(afterFrame)
 
   // open page
-  await page.goto(baseUrl("/"));
+  await page.goto(baseUrl("/"))
 
   // Validate elements are available in the DOM
-  await test.expect(page.locator("#run")).toBeVisible();
-  await test.expect(page.locator("#swaprows")).toBeVisible();
-  await test.expect(page.locator(".table > tbody")).toBeEmpty();
+  await test.expect(page.locator("#run")).toBeVisible()
+  await test.expect(page.locator("#swaprows")).toBeVisible()
+  await test.expect(page.locator(".table > tbody")).toBeEmpty()
 
   // Create 10000 rows
   await page.evaluate(() => {
-    document.getElementById("run").click();
-  });
+    document.getElementById("run").click()
+  })
   // wait for table to render and validate the number of rows
   await page
     .locator(
@@ -246,31 +258,34 @@ test("swap_rows", async ({ page }) => {
     )
     .waitFor({
       state: "attached",
-    });
-  await test.expect(page.locator(".table > tbody > tr")).toHaveCount(1000);
+    })
+  await test.expect(page.locator(".table > tbody > tr")).toHaveCount(1000)
   await test
     .expect(
       page.locator(
         ".table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)",
       ),
     )
-    .toContainText("2");
+    .toContainText("2")
   await test
     .expect(
       page.locator(
         ".table > tbody:nth-child(1) > tr:nth-child(999) > td:nth-child(1)",
       ),
     )
-    .toContainText("999");
+    .toContainText("999")
+
+  // Wait for a second so metrics are available
+  await page.waitForTimeout(1000)
 
   await page.evaluate(() => {
-    performance.mark("swap_rows_start");
-    document.getElementById("swaprows").click();
+    performance.mark("swap_rows_start")
+    document.getElementById("swaprows").click()
     // @ts-ignore: We are sure that the function is available in the window object
     window.afterFrame(() => {
-      performance.mark("swap_rows_end");
-    });
-  });
+      performance.mark("swap_rows_end")
+    })
+  })
 
   // check if 2nd and 999th rows are swapped
   await test
@@ -279,14 +294,14 @@ test("swap_rows", async ({ page }) => {
         ".table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)",
       ),
     )
-    .toContainText("999");
+    .toContainText("999")
   await test
     .expect(
       page.locator(
         ".table > tbody:nth-child(1) > tr:nth-child(999) > td:nth-child(1)",
       ),
     )
-    .toContainText("2");
+    .toContainText("2")
 
   // Calculate the duration of the action
   const measure = await page.evaluate(() => {
@@ -294,7 +309,7 @@ test("swap_rows", async ({ page }) => {
       "swap_rows_duration",
       "swap_rows_start",
       "swap_rows_end",
-    );
-  });
-  persistMeasure(page, measure);
-});
+    )
+  })
+  persistMeasure(page, measure)
+})
